a612eb54a42354d27a7ef0569185f0df4ea83267
kavishetty123 <145020104+kavishetty123@users.noreply.github.com>
2025-12-20 08:19:38 +0000

    adding basic requirement files

--- production_quality_airflow_etl/etl/extract.py
+++ production_quality_airflow_etl/etl/extract.py
@@ -1,12 +1,29 @@
 import pandas as pd
 
-DATA_DIR = "/opt/airflow/data"
 
 def extract_data():
-    X = pd.read_csv(f"{DATA_DIR}/data_X_sample.csv")
-    Y = pd.read_csv(f"{DATA_DIR}/data_Y_sample.csv")
+    # Load the CSV files
+    X = pd.read_csv(f"/workspaces/production-quality-control-airflow-etl/production_quality_airflow_etl/data/data_X.csv")
+    Y = pd.read_csv(f"/workspaces/production-quality-control-airflow-etl/production_quality_airflow_etl/data/data_Y.csv")
 
+    # Save raw copies
     X.to_csv("/tmp/X_raw.csv", index=False)
     Y.to_csv("/tmp/Y_raw.csv", index=False)
-    print("Extraction complete: X_raw.csv and Y_raw.csv created.")
 
+    # Print basic info for X
+    print("X_raw.csv created.")
+    print("Shape of X:", X.shape)
+    print("Columns in X:", list(X.columns))
+    print("First 5 rows of X:")
+    print(X.head(), "\n")
+
+    # Print basic info for Y
+    print("Y_raw.csv created.")
+    print("Shape of Y:", Y.shape)
+    print("Columns in Y:", list(Y.columns))
+    print("First 5 rows of Y:")
+    print(Y.head(), "\n")
+
+    print("Extraction complete.")
+# ✅ Call the function
+extract_data()
